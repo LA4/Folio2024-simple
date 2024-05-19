ade061c78c411fe2f500169ec5c0eb11a93bea8c
LA4 <andr.ludow@gmail.com>
2024-05-19 19:26:42 +0200

    thrid try

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@
 import "./globals.css";
 import Menu from "../../components/menu/Menu";
 import Head from "next/head";
-import { usePathname } from "next/navigation";
 
 
 
@@ -11,7 +10,7 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
   const pageDescription = `  Développeur Web spécialisé en React/redux/express - Création de sites web modernes et performants.  
 Bienvenue sur mon site ! Je suis un développeur web  passionné avec une expertise en React et une connaissance des
 langages tels que JavaScript, HTML5, et CSS3. Je me spécialise dans la création de sites web interactifs, applications web dynamiques, et interfaces utilisateur réactives.`
-  const router = usePathname()
+
   return (
     <html lang="fr">
       <Head>
@@ -22,8 +21,7 @@ langages tels que JavaScript, HTML5, et CSS3. Je me spécialise dans la créatio
         <link rel="icon" href="/public/lago_LA4.png" type="image/png" />
       </Head>
       <body >
-        {router !== '/' ? <Menu>{children}</Menu> : <>{children}</>
-        }
+        <Menu>{children}</Menu>
       </body>
     </html>
   );
