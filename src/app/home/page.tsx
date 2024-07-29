97b99389e7d56ad7f0181cd350ee0c3c291b4de8
LA4 <andr.ludow.com>
2024-07-29 10:14:11 +0200

    feat: animate background annimation

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,8 +2,38 @@
 import Tag from "../../../components/tags/Tag";
 import { MotionConfig, motion } from "framer-motion";
 import styles from "./home.module.css";
+import { transform } from "next/dist/build/swc";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+  const [perspectiveEffect, setPerspectiveEffect] = useState({
+    transform: `perspective(400px) rotateY(0)`,
+  });
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+      const xOffset = (e.clientX - windowWidth / 2) / (windowWidth / 2);
+      const yOffset = (e.clientY - windowHeight / 2) / (windowHeight / 2);
+      const rotationX = xOffset * 5;
+      const rotationY = yOffset * -8;
+
+      setPerspectiveEffect({
+        transform: `perspective(400px) rotateY(${rotationX}deg) rotateX(${rotationY}deg)`,
+      });
+    };
+
+    const throttledMouseMove = (e: MouseEvent) => {
+      requestAnimationFrame(() => handleMouseMove(e));
+    };
+
+    document.addEventListener("mousemove", throttledMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", throttledMouseMove);
+    };
+  }, []);
+
   return (
     <>
       <div className={styles.container}>
@@ -15,11 +45,13 @@ export default function HomePage() {
             className={styles.containerHome}
           >
             <h1 className={styles.homeTitle}>Andreotti Ludovic</h1>
-            <p className={styles.homeParagraph} style={{ userSelect: "none" }}>Décrouvrez mon univers à travers ces sites internet </p>
+            <p className={styles.homeParagraph} style={{ userSelect: "none" }}>
+              Décrouvrez mon univers à travers ces sites internet{" "}
+            </p>
           </motion.div>
         </MotionConfig>
         <span className={styles.contactTag}>
-          <Tag href="https://github.com/LA4" target="_blank" >
+          <Tag href="https://github.com/LA4" target="_blank">
             GitHub
           </Tag>
           <Tag
@@ -28,16 +60,10 @@ export default function HomePage() {
           >
             Linkedin
           </Tag>
-          <Tag
-            href="https://www.instagram.com/ludoandr4rt"
-            target="_blank"
-          >
+          <Tag href="https://www.instagram.com/ludoandr4rt" target="_blank">
             Instagram
           </Tag>
-          <Tag
-            href="https://ludraw.artstation.com/"
-            target="_blank"
-          >
+          <Tag href="https://ludraw.artstation.com/" target="_blank">
             Art Station
           </Tag>
         </span>
@@ -45,40 +71,48 @@ export default function HomePage() {
       <div className={styles.projectsContainer}>
         <div className={styles.projectItems}>
           <Tag
-            bgColor='#827964'
+            bgColor="#827964"
             color="white"
             href="https://la-4-design-v2.vercel.app/"
             target="_blank"
-          >La4-design</Tag>
-          <div>
-            Site internet static
-          </div>
+          >
+            La4-design
+          </Tag>
+          <div>Site internet static</div>
         </div>
 
         <div className={styles.projectItems}>
           <Tag
-            bgColor='#827964'
+            bgColor="#827964"
             color="white"
             href="https://drive.google.com/file/d/1Mop1NrKHN2BuCJS0UrK45KrPIpO8CeR4/view"
             target="_blank"
-          >LifeSync</Tag>
-          <div>
-            Apllication de journaling
-          </div>
+          >
+            LifeSync
+          </Tag>
+          <div>Apllication de journaling</div>
         </div>
         <div className={styles.projectItems}>
           <Tag
-            bgColor='#827964'
+            bgColor="#827964"
             color="white"
             href="https://mymoviz-frontend-plum-six.vercel.app/"
             target="_blank"
-          >My movie's</Tag>
+          >
+            My movie's
+          </Tag>
           <div>
-            Site interet permettant de voir les sorties cinémas et les notations associées
+            Site interet permettant de voir les sorties cinémas et les notations
+            associées
           </div>
         </div>
       </div>
-      <img className={styles.background} src="/images/bg-home.jpg" alt="Image de pokeballs faites en 3d part Andreotti ludovic" />
+      <img
+        style={perspectiveEffect}
+        className={styles.background}
+        src="/images/bg-home.jpg"
+        alt="Image de pokeballs faites en 3d part Andreotti ludovic"
+      />
     </>
   );
 }
