ed5d96911925313fef16079d71ff8bb4d8ca5300
LA4 <andr.ludow.com>
2024-07-29 13:31:06 +0200

    ff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ langages tels que JavaScript, HTML5, et CSS3. Je me spécialise dans la créatio
     <html lang="fr">
       <Head>
         <title>Andreotti Ludovic</title>
-        <link rel="icon" href="/lago_LA4.png" type="image/png" />
+        <link rel="icon" href="/public/lago_LA4.png" type="image/png" />
         <meta name="description" content={pageDescription} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="UTF-8" />
