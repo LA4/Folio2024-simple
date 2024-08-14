a932c210406261dd296f07aa2572091b3b4ca122
LA4 <andr.ludow.com>
2024-08-14 15:27:58 +0200

    presentation page with movie

---
+++ src/app/presentation/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import Tag from "../../../components/tags/Tag";
+import { MotionConfig, motion } from "framer-motion";
+import styles from "./home.module.css";
+import { transform } from "next/dist/build/swc";
+import { useEffect, useState } from "react";
+
+export default function HomePage() {
+
+  return (
+    <>
+      
+  );
+}
