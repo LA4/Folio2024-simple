3b9aa984c9969f6306b8a50af8cbe11fd196ca89
LA4 <andr.ludow.com>
2024-08-14 16:34:21 +0200

    add video and video source and some styles

--- src/app/presentation/page.tsx
+++ src/app/presentation/page.tsx
@@ -1,14 +1,17 @@
 "use client";
-import Tag from "../../../components/tags/Tag";
-import { MotionConfig, motion } from "framer-motion";
-import styles from "./home.module.css";
-import { transform } from "next/dist/build/swc";
-import { useEffect, useState } from "react";
-
+import styles from "./presentation.module.css";
 export default function HomePage() {
-
   return (
     <>
-      
+      <div className={styles.container}>
+        <span className={`${styles.button} ${styles.buttonBefore}`}>
+          before
+        </span>
+        <video autoPlay loop muted className={styles.videoPres}>
+          <source src="/videos/demo ThreadBook.mp4" type="video/mp4" />
+        </video>
+        <span className={`${styles.button} ${styles.buttonAfter}`}>after</span>
+      </div>
+    </>
   );
 }
